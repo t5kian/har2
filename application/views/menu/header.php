4ef9e73c3893a69becccdc8da34b679dd3207e75
Unknown <t5kian00@students.oamk.fi>
2017-11-02 12:57:12 +0200

    Tietokanta osio lisätty

---
+++ application/views/menu/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>CodeIgniter esimerkki</title>
+  </head>
+  <body>
+    <ul>
+      <li><a href="<?php echo site_url('');?>">Etusivu</a></li>
+      <li><a href="<?php echo site_url('test/kolmas');?>">Test/kolmas</a></li>
+      <li><a href="<?php echo site_url('test/neljas');?>">Test/neljas</a></li>
+      <li><a href="<?php echo site_url('oppilas/nayta_oppilaat');?>">Opiskelijat</a></li>
+    </ul>
